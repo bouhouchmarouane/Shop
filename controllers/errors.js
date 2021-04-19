0eaa4a9d5189bfe4d18241c9af0a5eff7c123f7e
marouanebhch <bouhouch.marouane@gmail.com>
2021-04-19 19:02:39 +0000

    Adding product model

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,6 +1,6 @@
 exports.get404 = (req, res) => {
     res.status(404).render('404', {
-        docTitle: '404 - Page not found',
+        pageTitle: '404 - Page not found',
         path: ''
     });
 }
--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,23 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res) => {
     res.render('add-product', {
-        docTitle: 'Add Product',
+        pageTitle: 'Add Product',
         path: '/admin/add-product'
     });
 }
 
 exports.postAddProduct = (req, res) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
 exports.getProducts = (req, res) => {
-    console.log('in the /');
+    const products = Product.fetchAll();
     res.render('shop', {
-        docTitle: 'Shop',
-        path: '/'
+        pageTitle: 'Shop',
+        path: '/',
+        products: products
     });
 }
