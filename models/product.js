15e3a6577369f1c779693718642c4d3db934dfa7
marouanebhch <bouhouch.marouane@gmail.com>
2021-04-25 12:04:55 +0000

    Defining product

--- models/product.js
+++ models/product.js
@@ -1,41 +1,24 @@
-const database = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-    constructor(title, price, imageUrl, description, id) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.description = description;
-    }
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: Sequelize.STRING,
+    descrition: Sequelize.STRING
+});
 
-    save() {
-        return database.execute('insert into products (title, price, description, imageUrl) values(?, ?, ?, ?)',
-            [this.title, this.price, this.description, this.imageUrl]);
-    }
-
-    static delete(productId, callback) {
-        // getProductsFromFile(products => {
-        //     const newProducts = products.filter(p => p.id !== productId);
-        //     Product.findById(productId, product => {
-        //         Cart.deleteProduct(product);
-        //         fs.writeFile(p, JSON.stringify(newProducts), error => {
-        //             if(error) {
-        //                 console.log(error);
-        //             }
-        //             callback();
-        //         });
-        //     });
-        // });
-    }
-
-    static fetchAll(callback) {
-        return database.execute('select * from products');
-    }
-
-    static findById(id) {
-        return database.execute('select * from products where id = ?', [id]);
-    }
-}
+module.exports = Product;
