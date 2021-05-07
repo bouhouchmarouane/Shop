077556a0d76b893bd5b18adf4e9fd8e9871a8b9a
marouanebhch <bouhouch.marouane@gmail.com>
2021-05-07 00:00:34 +0000

    Adding sign up page

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,6 +18,41 @@ exports.postLogin = (req, res) => {
         .catch(error => console.log(error));
 }
 
+exports.getSignup = (req, res) => {
+    res.render('auth/signup', {
+        pageTitle: 'Sign up',
+        path: '/auth/signup',
+        isLoggedIn: false
+    });
+}
+
+exports.PostSignup = (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    User.find({email: email})
+        .then(user => {
+            console.log(user.length > 0);
+            if(user.length > 0) {
+                console.log("1");
+                return res.redirect('/auth/signup');
+            }
+            console.log("2");
+            const newUser = new User({
+                name,
+                email,
+                password,
+                cart: []
+            });
+            console.log("3");
+            return newUser.save();
+        })
+        .then(() => {
+            console.log('4')
+            res.redirect('/auth/login')})
+        .catch(error => console.log(error));
+}
+
 exports.getLogout = (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const authController = require('../controllers/auth')
 
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.PostSignup);
 router.get('/logout', authController.getLogout);
 
 module.exports = router;
