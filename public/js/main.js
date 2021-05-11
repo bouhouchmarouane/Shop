90d979a8a3d12767d8dca11851d71b8e59264e93
Bouhouch Marouane <bouhouch.marouane@gmail.com>
2021-05-11 00:18:26 +0000

    sending delete request

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,12 @@
 const deleteProduct = (productId, csrf) => {
     console.log(productId);
     console.log(csrf);
+    fetch('/admin/product/' + productId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    })
+        .then(result => console.log(result))
+        .catch(error => console.log(error))
 }
